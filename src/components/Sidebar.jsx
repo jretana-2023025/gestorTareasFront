536cfdee2e5f0495e14106902c30f422f94debb6
JRetana-2023025 <jretana-2023025@kinal.edu.gt>
2025-05-18 22:58:33 -0600

    Create Sidebar.jsx

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Sidebar = () => {
+  return (
+    <div>
+      <h4 className='text-center'>Sidebar</h4>
+      <ul className='nav flex-column'>
+        <li className='nav-item'>
+          Home
+        </li>
+      </ul>
+    </div>
+  )
+}
+
